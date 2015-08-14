73f549d33d3a74e932ed3cc5d07ac2960725df3b
linostar <linux.anas@gmail.com>
2015-08-14 23:45:45 +0300

    Added basic arg parsing to main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
-
+import argparse
 
 def main():
-	pass
+	parser = argparse.ArgumentParser(description='Generate a visual HTML/CSS output from a JSON file based on a JSOV template')
+	parser.add_argument('-t', '--template', nargs=1, type=file)
+	parser.add_argument('--no-output', nargs=0, action='store_true')
+	parser.add_argument('--html-output', nargs=1, type=file)
+	parser.add_argument('--css-output', nargs=1, type=file)
 
 
 if __name__ == "__main__":
