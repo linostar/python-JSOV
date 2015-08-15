d96cc808fed25fa24c7a09308a9c87fda317eb24
linostar <linux.anas@gmail.com>
2015-08-15 16:31:29 +0300

    Update the arg parsing function

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # python-JSOV
 Python generator for JSOV (Javascript Object Visualization).
 
-This generator takes object(s) from a JSON file, and generate a visual representation of them, using HTML/CSS for example, based on a JVOC template. the JVOC template is responsible for how the object(s), along with their different children and parameters, will be graphically represented.
+This generator takes object(s) from a JSON file, and generate a visual representation of them, using HTML/CSS for example, based on a JSOV template. the JVOC template is responsible for how the object(s), along with their different children and parameters, will be graphically represented.
--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import argparse
 
 def main():
 	parser = argparse.ArgumentParser(description='Generate a visual HTML/CSS output from a JSON file based on a JSOV template')
-	parser.add_argument('-t', '--template', nargs=1, type=file)
-	parser.add_argument('--no-output', nargs=0, action='store_true')
-	parser.add_argument('--html-output', nargs=1, type=file)
-	parser.add_argument('--css-output', nargs=1, type=file)
+	parser.add_argument('-t', '--template', nargs=1, required=True, type=argparse.FileType('r'), help='the JSOV template file')
+	parser.add_argument('--no-output', action='store_true', help='do not write to any output files')
+	parser.add_argument('--html-output', nargs=1, type=argparse.FileType('w'), help='HTML output filename')
+	parser.add_argument('--css-output', nargs=1, type=argparse.FileType('w'), help='CSS output filename')
+	parser.add_argument('-f', '--format', nargs=1, default='html', help='the visual format for the output (HTML by default)')
+	parser.add_argument('inputfile', type=argparse.FileType('r'), help='input JSON file')
+	args = parser.parse_args()
 
 
 if __name__ == "__main__":
-	pass
+	main()
