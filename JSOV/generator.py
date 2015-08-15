06cfb7bdc80e683653525fef99936b9dd491a576
linostar <linux.anas@gmail.com>
2015-08-15 22:31:13 +0300

    Added yaml import for the JSOV template file

--- JSOV/generator.py
+++ JSOV/generator.py
@@ -1,14 +1,17 @@
 import json
 
+import yaml
+
 
 class Generator:
 	def __init__(self, jsonfile, jsovfile):
 		self.jsonfile = jsonfile
 		self.jsovfile = jsovfile
-		self.load_dict()
+		self.load_dicts()
 
-	def load_dict(self):
+	def load_dicts(self):
 		self.dict = json.load(self.jsonfile)
+		self.template = yaml.load(self.jsovfile)
 
 	def generate_html(self, output_html, output_css):
 		pass
