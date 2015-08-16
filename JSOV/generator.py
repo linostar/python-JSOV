129afc5f0b402436c54fd75baddc2e1a5491dd96
linostar <linux.anas@gmail.com>
2015-08-16 11:55:43 +0300

    Re-adjusted the jsov template sample

--- JSOV/generator.py
+++ JSOV/generator.py
@@ -1,3 +1,4 @@
+import os
 import json
 
 import yaml
@@ -6,12 +7,20 @@ import yaml
 class Generator:
 	def __init__(self, jsonfile, jsovfile):
 		self.jsonfile = jsonfile
-		self.jsovfile = jsovfile
+		self.jsovfile = jsovfile[0]
 		self.load_dicts()
 
 	def load_dicts(self):
-		self.dict = json.load(self.jsonfile)
-		self.template = yaml.load(self.jsovfile)
+		if not os.path.exists(self.jsonfile):
+			print("Error: inputfile '{}' could not be found.".format(self.jsonfile))
+			sys.exit(1)
+		if not os.path.exists(self.jsovfile):
+			print("Error: template '{}' could not be found.".format(self.jsovfile))
+			sys.exit(1)
+		with open(self.jsonfile, "r") as json_fp:
+			self.dict = json.load(json_fp)
+		with open(self.jsovfile, "r") as jsov_fp:
+			self.template = yaml.load(jsov_fp)
 
 	def generate_html(self, output_html, output_css):
 		pass
