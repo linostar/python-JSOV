7a5213ec6bad9e76394ef1c36c25826bab67216d
linostar <linux.anas@gmail.com>
2015-08-18 23:10:24 +0300

    Made Utils.lower_keys() also go through lists of dicts

--- JSOV/utils.py
+++ JSOV/utils.py
@@ -5,6 +5,11 @@ class Utils:
 		for key, val in dic.items():
 			if isinstance(dic[key], dict):
 				new_dic[str(key).lower()] = Utils.lower_keys(dic[key])
+			elif isinstance(dic[key], list):
+				dic_list = []
+				for item in dic[key]:
+					dic_list.append(Utils.lower_keys(item))
+				new_dic[str(key).lower()] = dic_list
 			else:
 				new_dic[str(key).lower()] = val
 		return new_dic
@@ -15,3 +20,5 @@ class Utils:
 			return string[0].upper() + string[1:]
 		elif len(string):
 			return string.upper()
+		else:
+			return ""
