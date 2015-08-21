a7662dd08010f196861cadabd368162cd94c607d
linostar <linux.anas@gmail.com>
2015-08-21 21:36:54 +0300

    Added more options to the main prog (--custom, --html-template, --css-template)

--- main.py
+++ main.py
@@ -13,14 +13,23 @@ def main():
 	parser.add_argument('--html-output', nargs=1, default='output.html', help='HTML output filename')
 	parser.add_argument('--css-output', nargs=1, default='style.css', help='CSS output filename')
 	parser.add_argument('-f', '--format', nargs=1, default='html', help='the visual format for the output (HTML by default)')
+	parser.add_argument('--custom', action='store_true', help='allows using --html-template and --css-template')
+	parser.add_argument('--html-template', nargs=1, help='uses a user-made custom html template instead of the built-in ones')
+	parser.add_argument('--css-template', nargs=1, help='uses a user-made custom css template instead of the built-in ones')
 	parser.add_argument('inputfile', help='input JSON file')
-	args = parser.parse_args()
-	visualizer = generator.Generator(args.inputfile, args.template)
-	if args.no_output:
-		print(visualizer.generate_htmlcss())
-	else:
-		visualizer.generate_htmlcss(args.html_output, args.css_output)
+	check_args(parser.parse_args())
 	
+def check_args(args):
+	if args.custom and args.html_template and args.css_template:
+		pass
+	elif args.custom:
+		print("Option '--custom' can only be used along with '--html-template' and '--css-template'")
+	else:
+		visualizer = generator.Generator(args.inputfile, args.template)
+		if args.no_output:
+			print(visualizer.generate_htmlcss())
+		else:
+			visualizer.generate_htmlcss(args.html_output, args.css_output)
 
 if __name__ == "__main__":
 	main()
