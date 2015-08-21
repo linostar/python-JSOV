f8da0fa6d159863b8a1364ddb0511fdaada2baab
linostar <linux.anas@gmail.com>
2015-08-21 21:55:08 +0300

    Added some restrictions on using the mutually exclusive options

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ def main():
 	parser = argparse.ArgumentParser(description='Generate a visual HTML/CSS output from a JSON file based on a JSOV template')
 	parser.add_argument('-t', '--template', nargs=1, required=True, help='the JSOV template file')
 	parser.add_argument('--no-output', action='store_true', help='do not write to any output files')
-	parser.add_argument('--html-output', nargs=1, default='output.html', help='HTML output filename')
-	parser.add_argument('--css-output', nargs=1, default='style.css', help='CSS output filename')
+	parser.add_argument('--html-output', nargs=1, help='HTML output filename')
+	parser.add_argument('--css-output', nargs=1, help='CSS output filename')
 	parser.add_argument('-f', '--format', nargs=1, default='html', help='the visual format for the output (HTML by default)')
 	parser.add_argument('--custom', action='store_true', help='allows using --html-template and --css-template')
 	parser.add_argument('--html-template', nargs=1, help='uses a user-made custom html template instead of the built-in ones')
@@ -23,13 +23,19 @@ def check_args(args):
 	if args.custom and args.html_template and args.css_template:
 		pass
 	elif args.custom:
-		print("Option '--custom' can only be used along with '--html-template' and '--css-template'")
+		print("Error: Option '--custom' can only be used along with '--html-template' and '--css-template'.")
+		sys.exit(1)
 	else:
 		visualizer = generator.Generator(args.inputfile, args.template)
-		if args.no_output:
+		if args.no_output and not args.html_output and not args.css_output:
 			print(visualizer.generate_htmlcss())
+		elif args.no_output:
+			print("Error: Option '--no-output' does not allow '--html-output' or '--css-output'.")
+			sys.exit(1)
 		else:
-			visualizer.generate_htmlcss(args.html_output, args.css_output)
+			html_out = args.html_output if args.html_output else "output.html"
+			css_out = args.css_output if args.css_output else "style.css"
+			visualizer.generate_htmlcss(html_out, css_out)
 
 if __name__ == "__main__":
 	main()
