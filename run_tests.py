1968f5e6161afb43fb26ba1ca1a125587d282c9e
linostar <linux.anas@gmail.com>
2015-08-27 12:09:58 +0300

    Fixed variable names in run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -10,23 +10,23 @@ class Run_Tests(unittest.TestCase):
 	OUTPUT_HTML = "tests/output/output.html"
 	OUTPUT_CSS = "tests/output/style.css"
 	GENERATED_DIR = "tests/generated/"
-	GENERATED_HTML = GENERATED_DIR + "output.html"
-	GENERATED_CSS = GENERATED_DIR + "style.css"
+	GENERATED_HTML = "tests/generated/output.html"
+	GENERATED_CSS = "tests/generated/style.css"
 
 	def test_input_json_exists(self):
-		self.assertTrue(os.path.exists(INPUT_JSON))
+		self.assertTrue(os.path.exists(self.INPUT_JSON))
 
 	def test_input_jsov_exists(self):
-		self.assertTrue(os.path.exists(INPUT_TEMPLATE))
+		self.assertTrue(os.path.exists(self.INPUT_TEMPLATE))
 
 	def test_output_html_exists(self):
-		self.assertTrue(os.path.exists(OUTPUT_HTML))
+		self.assertTrue(os.path.exists(self.OUTPUT_HTML))
 
 	def test_output_css_exists(self):
-		self.assertTrue(os.path.exists(OUTPUT_CSS))
+		self.assertTrue(os.path.exists(self.OUTPUT_CSS))
 
 	def test_generated_dir_exists(self):
-		self.assertTrue(os.path.exists(GENERATED_DIR))
+		self.assertTrue(os.path.exists(self.GENERATED_DIR))
 
 
 if __name__ == "__main__":
