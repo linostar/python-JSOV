c6236e9934322ad977f6f3224e2f4d95799f1c28
linostar <linux.anas@gmail.com>
2015-09-07 17:56:03 +0300

    Using django-style template variables

--- JSOV/parse.py
+++ JSOV/parse.py
@@ -16,9 +16,9 @@ class Parse:
 		for line in lines:
 			num_line += 1
 			indent = next_indent
-			if line.startswith("{{for "):
+			if line.startswith("{% for ") and line.endswith(" %}"):
 				next_indent += "\t"
-				for_var = line[5:-2].strip()
+				for_var = line[6:-2].strip()
 				if for_var.isdigit():
 					line = "for i{0} in range({1}):".format(str(num_line), for_var)
 				else:
@@ -37,37 +37,39 @@ class Parse:
 							pass
 					except Exception:
 						print("Error: You can only use 'children.x' or 'children.x.value' where x is a number.")
-			elif line == "{{endfor}}":
+			elif line == "{% endfor %}":
 				next_indent = indent[:-1]
 				continue
-			elif line.startswith("{{if") and line.endswith("}}"):
+			elif line.startswith("{% if") and line.endswith(" %}"):
 				pass
-			elif line == "{{endif}}":
+			elif line.startswith("{% elif") and line.endswith(" %}"):
+				pass
+			elif line == "{% else %}":
+				pass
+			elif line == "{% endif %}":
 				pass
 			else:
 				line = "Parse.templated_html += \"\"\"{}\"\"\"".format(line)
 			format_arr = []
-			matched_var = re.search(r"({children\.(\d+)})", line)
+			matched_var = re.search(r"({{\schildren\.(\d+) }})", line)
 			if matched_var:
 				child_depth = matched_var.group(2)
 				line = line.replace(matched_var.group(1), "{children" + str(child_depth) + "}")
-				#line += ".format({0}={0})".format("children" + str(child_depth))
 				format_arr.append(("children" + str(child_depth), "children" + str(child_depth)))
-			matched_var = re.search(r"({children\.(\d+)\.value})", line)
+			matched_var = re.search(r"({{ children\.(\d+)\.value }})", line)
 			if matched_var:
 				child_depth = int(matched_var.group(2))
 				child_val = "json_obj[root]"
 				for k in range(1, child_depth+1):
 					child_val += "[children{}]".format(k)
 				line = line.replace(matched_var.group(1), "{children" + str(child_depth) + "val}")
-				#line += ".format({0}={1})".format("children" + str(child_depth) + "val", child_val)
 				format_arr.append(("children" + str(child_depth) + "val", child_val))
 			if len(format_arr) == 1:
 				line += ".format({}={})".format(format_arr[0][0], format_arr[0][1])
 			elif len(format_arr) == 2:
 				line += ".format({}={}, {}={})".format(format_arr[0][0], format_arr[0][1],
 					format_arr[1][0], format_arr[1][1])
-			line = line.replace("{root}", root)
+			line = line.replace("{{ root }}", root)
 			block += indent + line + "\n"
 		exec(block)
 		return Parse.templated_html
